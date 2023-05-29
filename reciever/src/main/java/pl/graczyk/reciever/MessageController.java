2db0d63eeed19c682526a24e133e9ab4a6d8a4ca
ngraczyk <natalia.graczyk@dxc.com>
2023-05-29 10:50:32 +0200

    implement basic rabbit listener

--- reciever/src/main/java/pl/graczyk/reciever/MessageController.java
+++ reciever/src/main/java/pl/graczyk/reciever/MessageController.java
@@ -1,5 +1,6 @@
 package pl.graczyk.reciever;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class MessageController {
     } else {
             return "Nie ma nowych wiadomości";
         }
+    }
+
+    @RabbitListener(queues = "kurs")
+    public void messageListener(String message) {
+        System.out.println(message);
     }
 }
