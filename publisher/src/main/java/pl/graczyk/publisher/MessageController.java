28dc070736c28ac3fdc2c1a0b68e613c5d9900ac
ngraczyk <natalia.graczyk@dxc.com>
2023-05-29 10:41:56 +0200

    create basic publisher and reciever app

---
+++ publisher/src/main/java/pl/graczyk/publisher/MessageController.java
@@ -0,0 +1,25 @@
+package pl.graczyk.publisher;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+
+    public MessageController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @GetMapping("/message")
+    public String sendMessage(@RequestParam String message) {
+        rabbitTemplate.convertAndSend("kurs", message);
+        return "Wrzucono wiadomość na RabbitMq";
+        // localhost:8080/message?message=aaaa
+    }
+}
---
+++ reciever/src/main/java/pl/graczyk/reciever/MessageController.java
@@ -0,0 +1,27 @@
+package pl.graczyk.reciever;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.ldap.PagedResultsControl;
+
+@RestController
+public class MessageController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @GetMapping("/message")
+    public String recieveMessage() {
+        Object message = rabbitTemplate.receiveAndConvert("kurs");
+        if(message != null) {
+        return "Udało się pobrać wiadomość: " + message.toString();
+    } else {
+            return "Nie ma nowych wiadomości";
+        }
+    }
+}
